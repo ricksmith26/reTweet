ba29df65fca391499b848b05ff3b366e56e8a9e3
Tom <TomSimmons@Toms-MacBook-Pro.local>
2018-05-17 16:30:16 +0100

    managed to read 50 tweets from an array of users

---
+++ main.js
@@ -0,0 +1,61 @@
+const https = require("https");
+const URL = "https://northwitter-api-wqhhzdeecj.now.sh/handles";
+const Twit = require("twit");
+const {
+  consumer_key,
+  consumer_secret,
+  access_token,
+  access_token_secret
+} = require("./config.js");
+
+let ncHandles = [];
+
+https.get(URL, response => {
+  let body = "";
+  response.on("data", packet => {
+    body += packet;
+  });
+  response.on("end", () => {
+    ncHandles = JSON.parse(body).handles;
+    console.log(ncHandles);
+    fiftyTweets(ncHandles, (err, arr) => {
+      //console.log(arr);
+    });
+  });
+});
+
+const T = new Twit({
+  consumer_key: consumer_key,
+  consumer_secret: consumer_secret,
+  access_token: access_token,
+  access_token_secret: access_token_secret
+});
+
+//
+//not broken above this point
+//
+
+function fiftyTweets(arr, cb) {
+  const result = [];
+  let count = 0;
+  arr.forEach((handle, index) => {
+    T.get(
+      "statuses/user_timeline",
+      { screen_name: handle, count: 50 },
+      (err, data, response) => {
+        if (Array.isArray(data)) {
+          count++;
+          let arr = [];
+          for (let i = 0; i < data.length; i++) {
+            arr.push(data[i].text);
+          }
+          result.push(arr);
+        }
+
+        if (count === arr.length - 1) cb(err, console.log(result));
+      }
+      //const tweets = data.map(tweetObj => tweetObj.text);
+      //console.log(tweets);
+    );
+  });
+}
